b28660a99b3ff2f78d93aac8dac5330f8a0a871b
Figamus <fig74656@yahoo.com>
2018-12-04 11:12:58 -0600

    Displaying Top20 to homepage index

--- Bangazon/Controllers/HomeController.cs
+++ Bangazon/Controllers/HomeController.cs
@@ -5,14 +5,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Bangazon.Models;
+using Bangazon.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Bangazon.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Product.Include(p => p.ProductType).Include(p => p.User).Take(20);
+            return View(await applicationDbContext.ToListAsync());
         }
 
         public IActionResult About()
