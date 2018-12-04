770aac89da2c78563436daa2b9900b7898bab728
Mark Hale <mark.hale94@gmail.com>
2018-12-04 11:09:33 -0600

    added the functionality that changes the quantity of a product when a user selects to add it to their order

--- Bangazon/Controllers/ProductsController.cs
+++ Bangazon/Controllers/ProductsController.cs
@@ -205,9 +205,12 @@ namespace Bangazon.Controllers
 			productToAddToOrder.OrderId = activeOrder.OrderId;
 			_context.Add(productToAddToOrder);
 			await _context.SaveChangesAsync();
-			//Subtract from the product's listed quantity when the order has been confirmed to avoid maximum suckiness
-			Product updatedProduct = new Product();
-			updatedProduct.ProductId = id;
+
+			/*Subtract from the product's listed quantity when the order has been confirmed to avoid maximum suckiness Take the product that we found above, subtract 1 from the quantity field, update the context, save the changes to complete the update.*/
+			productToAdd.Quantity = productToAdd.Quantity - 1;
+			_context.Update(productToAdd);
+			await _context.SaveChangesAsync();
+
 			return RedirectToAction("Details","Orders", new { id = openOrder.OrderId });
 		}
 	}
