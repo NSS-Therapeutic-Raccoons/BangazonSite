7f763049137861b095c384060820e83e9debf24b
Mark Hale <mark.hale94@gmail.com>
2018-12-05 14:34:50 -0600

    added a section to remove all orders associated with the given payment type

--- Bangazon/Controllers/PaymentTypesController.cs
+++ Bangazon/Controllers/PaymentTypesController.cs
@@ -30,29 +30,10 @@ namespace Bangazon.Views
 		public async Task<IActionResult> Index()
 		{
 			var user = await GetCurrentUserAsync();
-			var applicationDbContext = _context.PaymentType.Include(p => p.User)
+			var applicationDbContext = _context.PaymentType
+			.Include(p => p.User)
 			.Where(p => p.UserId == user.Id);
 			return View(await applicationDbContext.ToListAsync());
-		}
-
-		// GET: PaymentTypes/Details/5
-		[Authorize]
-		public async Task<IActionResult> Details(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var paymentType = await _context.PaymentType
-				.Include(p => p.User)
-				.FirstOrDefaultAsync(m => m.PaymentTypeId == id);
-			if (paymentType == null)
-			{
-				return NotFound();
-			}
-
-			return View(paymentType);
 		}
 
 		// GET: PaymentTypes/Create
@@ -85,60 +66,7 @@ namespace Bangazon.Views
 			return View(paymentType);
 		}
 
-		// GET: PaymentTypes/Edit/5
-		[Authorize]
-		public async Task<IActionResult> Edit(int? id)
-		{
-			if (id == null)
-			{
-				return NotFound();
-			}
-
-			var paymentType = await _context.PaymentType.FindAsync(id);
-			if (paymentType == null)
-			{
-				return NotFound();
-			}
-			ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", paymentType.UserId);
-			return View(paymentType);
-		}
-
-		// POST: PaymentTypes/Edit/5
-		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-		[HttpPost]
-		[Authorize]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> Edit(int id, [Bind("PaymentTypeId,DateCreated,Description,AccountNumber,UserId")] PaymentType paymentType)
-		{
-			if (id != paymentType.PaymentTypeId)
-			{
-				return NotFound();
-			}
-
-			if (ModelState.IsValid)
-			{
-				try
-				{
-					_context.Update(paymentType);
-					await _context.SaveChangesAsync();
-				}
-				catch (DbUpdateConcurrencyException)
-				{
-					if (!PaymentTypeExists(paymentType.PaymentTypeId))
-					{
-						return NotFound();
-					}
-					else
-					{
-						throw;
-					}
-				}
-				return RedirectToAction(nameof(Index));
-			}
-			ViewData["UserId"] = new SelectList(_context.ApplicationUsers, "Id", "Id", paymentType.UserId);
-			return View(paymentType);
-		}
+		
 
 		// GET: PaymentTypes/Delete/5
 		[HttpPost]
@@ -149,25 +77,20 @@ namespace Bangazon.Views
 
 			var paymentType = await _context.PaymentType
 				.Include(p => p.User)
+				.Include(p => p.Orders)
 				.FirstOrDefaultAsync(m => m.PaymentTypeId == id && m.UserId == user.Id);
-
+			if (paymentType.Orders != null)
+			{
+				foreach (Order order in paymentType.Orders)
+				{
+					_context.Remove(order);
+				}
+			}
 			_context.PaymentType.Remove(paymentType);
 			await _context.SaveChangesAsync();
 			return RedirectToAction("Index", "PaymentTypes");
 		}
 
-		// POST: PaymentTypes/Delete/5
-		/*[HttpPost, ActionName("Delete")]
-		[Authorize]
-		[ValidateAntiForgeryToken]
-		public async Task<IActionResult> DeleteConfirmed(int id)
-		{
-			var paymentType = await _context.PaymentType.FindAsync(id);
-			_context.PaymentType.Remove(paymentType);
-			await _context.SaveChangesAsync();
-			return RedirectToAction(nameof(Index));
-		}
-		*/
 		private bool PaymentTypeExists(int id)
 		{
 			return _context.PaymentType.Any(e => e.PaymentTypeId == id);
